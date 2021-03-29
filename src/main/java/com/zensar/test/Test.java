349819898f616d0834fcf3b1ef502c8d5d7ca72e
MuzikaMkhari <muzimkhari@yahoo.co.za>
2021-03-29 13:21:07 +0200

    updated test class

--- src/main/java/com/zensar/test/Test.java
+++ src/main/java/com/zensar/test/Test.java
@@ -3,7 +3,7 @@ package com.zensar.test;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Zensar Techfair");
+		System.out.println("Hello Zensar Techfair 2020");
 	}
 
 }
